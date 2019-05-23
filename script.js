3ed7f083cc85bdfcc3861c85b970e543cd8c2758
crongle <gasmazk73@gmail.com>
2019-05-23 17:36:45 +0100

    Add double digit plus functionality

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 // variables
 
 let oaoStack = [];
+let numCheck = /[0-9]/;
 let i = 0;
-    n = 1;
+    n = 0;
+    x = 0;
 
 
 
 // MAIN FUNCTION
 
 function operate() { // Performs full calculation on OaO stack
+
+  joinNumbers();
+
   for (n==1; n<i; n++) {
     switch (oaoStack[n]) {
       case "MULTIPLY": multiply(n); break;
+      case "DIVIDE": divide(n); break;
     }
   }
 }
@@ -20,7 +26,9 @@ function operate() { // Performs full calculation on OaO stack
 
 function divide(index) { //find number before and after add in OaO array and divide first by last and shrink array 3-1
   // add error message for diving by 0
-
+  let answer = oaoStack[index-1] / oaoStack[index+1];
+  oaoStack[index+1] = answer;
+  oaoStack.splice((index-1), 2);
 }
 
 function multiply(index) { //find number before and after add in OaO array and multiply them and shrink array 3-1
@@ -40,10 +48,27 @@ function subtract() { //find number before and after add in OaO array and subtra
 
 // Other calculation operators
 
-function addToStack(buttonInput) {
+function addToStack(buttonInput) { // Add value of button pressed into the OaO array
   oaoStack[i] = buttonInput;
   i+= 1;
 }
+
+function joinNumbers() { // Join together concurrent digits in oaoStack to form actual number
+  let sum = "";
+  let digitCount = 0;
+
+  for (x==0; x<i; x++) {
+   if (numCheck.test(oaoStack[x])) {
+     sum += oaoStack[x].toString();
+     digitCount += 1;
+   } else {
+     oaoStack[x-1] = parseInt(sum);
+     oaoStack.splice((x-digitCount), digitCount-1)
+     x++; // Prevent looping over the same values already joined
+   }
+  }
+}
+
 
 function clear() { // Clears display and current OaO stack
 
