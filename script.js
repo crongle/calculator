2c83409ff4df145c9bf86e11c4aff0bcb718b6b5
crongle <gasmazk73@gmail.com>
2019-05-23 12:03:04 +0100

    Add initial framework files

---
+++ script.js
@@ -0,0 +1,58 @@
+// variables
+
+let oaoStack = [];
+
+
+
+// MAIN FUNCTION
+
+function main() {
+
+}
+
+
+
+// Main 4 arithmatic operators
+
+function divide() { //find number before and after add in OaO array and divide first by last and shrink array 3-1
+  // add error message for diving by 0
+
+}
+
+function multiply() { //find number before and after add in OaO array and multiply them and shrink array 3-1
+
+}
+
+function add() { //find number before and after add in OaO array and add them and shrink array 3-1
+
+}
+
+function subtract() { //find number before and after add in OaO array and subtract them and shrink array 3-1
+
+}
+
+
+// Other calculation operators
+
+function operate() { // Performs full calculation on OaO stack
+
+}
+
+function clear() { // Clears display and current OaO stack
+
+}
+
+function backspace() { // Removes previous number key or operator entry from display and OaO stack
+
+}
+
+function decimalise() { // Takes numbers and decimal between operators and combines to floating point number in stack
+
+}
+
+
+// Display functions
+
+function updateDisplay() { //Called on any calculation update to output current OaO array
+
+}
